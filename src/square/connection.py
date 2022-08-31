a0e7e08e066780efa11aba6b866a990b2f6bcb71
chromapid <67613147+chromapid@users.noreply.github.com>
2022-08-31 13:21:20 -0600

    Connection mk 2

--- src/square/connection.py
+++ src/square/connection.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from square.client import Client
+from square.client import Client, CatalogApi
 
 class ConnectionEnvironment(Enum):
     SANDBOX = 'sandbox'
@@ -8,12 +8,20 @@ class ConnectionEnvironment(Enum):
 class SquareConnection:
     def __init__(self, access_token: str, environment: ConnectionEnvironment):
         self.__client = Client(access_token=access_token, environment=environment)
+        self.__catalog: CatalogApi = self.__client.catalog
 
     def get_square_client(self):
         return self.__client
 
-class Catalog
-    
+    def pull_catalog(self):
+        self.__catalog.list_catalog()
+
+    def push_catalog(self, catalog):
+        pass
+
+
+
+x = SquareConnection()
 
     
 
