ded981e98341c40853e7b7461e7e871e1ee5e13b
Joren24 <112424051+SorenTurner@users.noreply.github.com>
2022-08-31 13:08:06 -0600

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,1 +1,15 @@
+import csv
+import json
+
+csvfile = open('file.csv', 'r')
+jsonfile = open('file.json', 'w')
+
+fieldnames = ()
+reader = csv.DictReader( csvfile, fieldnames)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
+with open('file.json', 'r') as fs:
+    line=fs.readlines()
+    print(line)
 
