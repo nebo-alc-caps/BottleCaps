35631a1bfc5b354e63bba3800bb0c78c2c3df65a
chromapid <67613147+chromapid@users.noreply.github.com>
2022-09-02 12:53:12 -0600

    Stringification

--- src/square/connection.py
+++ src/square/connection.py
@@ -7,14 +7,38 @@ class ConnectionEnvironment(Enum):
 
 
 class CatalogObjectType(Flag):
-    ITEM = auto()
-    ITEM_VARIATION = auto()
-    MODIFIER = auto()
-    MODIFIER_LIST = auto()
-    CATEGORY = auto()
-    DISCOUNT = auto()
-    TAX = auto()
-    IMAGE = auto()
+    ITEM = 1 << 0
+    ITEM_VARIATION = 1 << 1
+    MODIFIER = 1 << 2
+    MODIFIER_LIST = 1 << 3
+    CATEGORY = 1 << 4
+    DISCOUNT = 1 << 5
+    TAX = 1 << 6
+    IMAGE = 1 << 7
+
+
+    __object_type_strings = [
+        "ITEM", 
+        "ITEM_VARIATION", 
+        "MODIFIER", 
+        "MODIFIER_LIST", 
+        "CATEGORY",
+        "DISCOUNT",
+        "TAX",
+        "IMAGE"
+    ]
+
+    __string_object_types = {
+        v: (1 << k) for k, v in enumerate(__object_type_strings)
+    }
+
+    def stringify(self):
+        actives = []
+        for i, v in enumerate(CatalogObjectType.__object_type_strings):
+            if self & (1 << i):
+                actives.append(v)
+        return ", ".join(actives)
+
 
 
 
@@ -31,11 +55,6 @@ class SquareConnection:
 
     def push_catalog(self, catalog):
         pass
-
-
-
-x = SquareConnection()
-
     
 
 
