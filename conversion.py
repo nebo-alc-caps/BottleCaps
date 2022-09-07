63f2705bd54d39325b9035511f9f13006812f07b
midnightsloth <90055959+midnightsloth@users.noreply.github.com>
2022-09-07 12:27:42 -0600

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -17,3 +17,19 @@ class Cata:
     def __init__(self,base,modifier):
         self.base=base
         self.modifier=modifier
+
+
+        #converter
+csvfile = open('file.csv', 'r')
+jsonfile = open('file.json', 'w')
+
+fieldnames = ()
+reader = csv.DictReader( csvfile, fieldnames)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
+with open('file.json', 'r') as fs:
+    line=fs.readlines()
+    print(line)
+    
+    
