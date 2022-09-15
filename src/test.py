39cf4fc8405ed48288df9195343c66dc1305a7d7
SmallPotato42 <112424087+SmallPotato42@users.noreply.github.com>
2022-09-15 13:25:20 -0600

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,6 +3,6 @@ Filepath = r'F:\ALCBottleCaps\BottleCaps\src\square\Options.json'
 
 with open(Filepath,'r',encoding="utf-8") as rf:
     test1 = json.loads(rf.read())
-#     Loops through Sizes object and prints the values stored
+#     Loops through Sizes(Json) object and prints the values stored
     for i in test1["Sizes"]:
         print(test1["Sizes"][i])
